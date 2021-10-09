a48bce2bb9e98580297172457d3ec4bc78eb0532
Roman Volodin <roman.volodin@gmail.com>
2021-10-10 01:30:52 +0300

    Add custom User model

--- paprika/settings.py
+++ paprika/settings.py
@@ -19,6 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -79,3 +81,5 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
