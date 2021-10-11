a682982945988a2fe319e88c4125b562333c6998
Roman Volodin <roman.volodin@gmail.com>
2021-10-11 23:16:25 +0300

    Add base.html

--- paprika/settings.py
+++ paprika/settings.py
@@ -38,7 +38,9 @@ ROOT_URLCONF = 'paprika.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
