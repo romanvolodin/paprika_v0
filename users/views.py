08fba38d133d5273bded8641bcbc1c6bd7d3b8af
Roman Volodin <roman.volodin@gmail.com>
2021-10-11 17:49:40 +0300

    Add log in and log out

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def login_user(request):
+    if request.user.is_authenticated:
+            return redirect('index')
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.success(request, 'Email or password is invalid.')
+            return redirect('login')
+
+    return render(request, 'users/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
