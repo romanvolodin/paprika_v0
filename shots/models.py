bd0d8d0f7e7bcdcf1b16ff7230fc37ac20bfa934
Roman Volodin <roman.volodin@gmail.com>
2021-10-12 10:52:19 +0300

    Add ShotGroup model

--- shots/models.py
+++ shots/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ShotGroup(models.Model):
+    title = models.CharField('Название группы шотов', max_length=50)
+    project = models.ForeignKey(
+        'projects.Project',
+        on_delete=models.CASCADE,
+        related_name='shot_groups'
+    )
+
+    def __str__(self) -> str:
+        return self.title
