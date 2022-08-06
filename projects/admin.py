82fe48bbf92b890e233ff02aaf9ee98dd57f8816
Roman Volodin <roman.volodin@gmail.com>
2022-08-06 16:47:00 +0300

    Setup admin page for Project list

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Project
 
 
-admin.site.register(Project)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "project_preview",
+        "code",
+        "title",
+        "deadline",
+    )
+
+    def project_preview(self, obj):
+        if obj.preview:
+            return mark_safe(f'<img src="{obj.preview.url}" style="width: 214px; height:135px;" />')
+        else:
+            return 'No preview'
+    project_preview.short_description = 'Image'
