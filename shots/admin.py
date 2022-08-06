77e1e9329e3d24d1498e9cec8e5017a6f83c71b4
Roman Volodin <roman.volodin@gmail.com>
2022-08-06 18:17:17 +0300

    Show latest version on Shot list

--- shots/admin.py
+++ shots/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import ShotGroup, Shot, Version
 
@@ -13,5 +14,12 @@ class ShotAdmin(admin.ModelAdmin):
         "group",
         "created_by",
         "created_at",
+        "get_latest_version",
     )
     list_filter = ("group__project", "group",)
+
+    @admin.display(description='Latest version')
+    def get_latest_version(self, obj):
+        latest_version = obj.versions.latest("created_at")
+        if latest_version:
+            return mark_safe(f'<a href="{latest_version.video.url}">{latest_version.title}</a>')
