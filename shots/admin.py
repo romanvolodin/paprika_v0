43a4bbfb214d3cc3ccdbbaf133decd416dc3a060
Roman Volodin <roman.volodin@gmail.com>
2022-08-06 17:24:35 +0300

    Setup admin page for Shot list

--- shots/admin.py
+++ shots/admin.py
@@ -4,5 +4,14 @@ from .models import ShotGroup, Shot, Version
 
 
 admin.site.register(ShotGroup)
-admin.site.register(Shot)
 admin.site.register(Version)
+
+@admin.register(Shot)
+class ShotAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "group",
+        "created_by",
+        "created_at",
+    )
+    list_filter = ("group__project", "group",)
